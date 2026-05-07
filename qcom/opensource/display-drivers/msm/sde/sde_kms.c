a0583e04dc80e49ece31681fbfc60934811db647
Alhaidar Latif <ziyalhaidar@gmail.com>
2026-05-07 18:35:41 +0000

    display-drivers/sde_kms: remove dcvs boost dependency from precommit
    Signed-off-by: Alhaidar Latif <ziyalhaidar@gmail.com>

--- qcom/opensource/display-drivers/msm/sde/sde_kms.c
+++ qcom/opensource/display-drivers/msm/sde/sde_kms.c
@@ -70,7 +70,6 @@
 #include "mi_dsi_display.h"
 
 #include <linux/cpu_boost.h>
-#include <soc/qcom/dcvs_boost.h>
 
 /* defines for secure channel call */
 #define MEM_PROTECT_SD_CTRL_SWITCH 0x18
@@ -1202,7 +1201,6 @@ static void sde_kms_prepare_commit(struct msm_kms *kms,
 		break;
 	case 3:
 		cpu_boost_kick(6);
-		qcom_dcvs_bus_boost_kick(6);
 		break;
 	default:
 		cpu_boost_kick(6);
